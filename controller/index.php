e382096009eef0d71e948458b6b2a964f7d5a7a4
softwareelves <softwareelves@87b157c0-a1aa-11de-923e-0b953e4d95d6>
2009-09-16 03:32:03 +0000

    Commit the basic question alg. Get system working again.
    git-svn-id: http://20-questions.googlecode.com/svn/trunk@11 87b157c0-a1aa-11de-923e-0b953e4d95d6

--- controller/index.php
+++ controller/index.php
@@ -1,10 +1,7 @@
 <?php
-echo 'kendall';
+
 $smarty = Common::getSmarty();
-echo 'boo';
-//$smarty->assign( "title" , "20 Questions" );
-echo 'balls';
-$smarty->display( "index2345.tpl" );
-echo 'sick';
+$smarty->assign( "title" , "20 Questions" );
+$smarty->display( "index.tpl" );
 
 ?>
