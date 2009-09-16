5067efcb4435ca613a3357b3dedd28ce418307cf
softwareelves <softwareelves@87b157c0-a1aa-11de-923e-0b953e4d95d6>
2009-09-16 04:22:35 +0000

    Pass in dynamic group_array for index.tpl to style.
    git-svn-id: http://20-questions.googlecode.com/svn/trunk@16 87b157c0-a1aa-11de-923e-0b953e4d95d6

--- controller/index.php
+++ controller/index.php
@@ -1,7 +1,21 @@
 <?php
 
+$sqloo = Common::getSqloo();
+$query = $sqloo->newQuery();
+$group_ref = $query->table( "group" );
+$query->column = array(
+	"id" => $group_ref->id,
+	"name" => $group_ref->name,	
+);
+$query->run();
+$group_array = array();
+foreach( $query as $row ){
+	$group_array[ $row["id"] ] = $row["name"];
+}
+
 $smarty = Common::getSmarty();
 $smarty->assign( "title" , "20 Questions" );
+$smarty->assign( "group_array", $group_array );
 $smarty->display( "index.tpl" );
 
 
