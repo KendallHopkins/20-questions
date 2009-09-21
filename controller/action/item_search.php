83c9499bd2926869ee865e5012bbfe92d7aad19e
softwareelves <softwareelves@87b157c0-a1aa-11de-923e-0b953e4d95d6>
2009-09-21 23:00:10 +0000

    Fix search.
    git-svn-id: http://20-questions.googlecode.com/svn/trunk@27 87b157c0-a1aa-11de-923e-0b953e4d95d6

--- controller/action/item_search.php
+++ controller/action/item_search.php
@@ -1,18 +1,13 @@
 <?php
 
-if( ( ! array_key_exists( "game_id", $_REQUEST ) ) ||
-	( ! array_key_exists( "search", $_REQUEST ) )
-) {
+if( ! array_key_exists( "search", $_REQUEST ) ) {
 	Common::sendJSON( array( "success" => FALSE, "error" => "Bad input" ) );
 }
 
 $sqloo = Common::getSqloo();
 $query = $sqloo->newQuery();
-$game_ref = $query->table( "game" );
-$group_ref = $game_ref->joinParent( "group", "group_id" );
-$item_ref = $group_ref->joinChild( "item", "group_id" );
+$item_ref = $query->table( "item" );
 
-$query->where[] = "$game_ref->id = ".$query->parameter( $_REQUEST["game_id"] );
 $query->where[] = "$item_ref->name LIKE ".$query->parameter( $_REQUEST["search"]."%" );
 
 $query->column = array(
@@ -21,6 +16,7 @@ $query->column = array(
 );
 
 $query->limit = 10;
+$query->order[$item_ref->name] = Sqloo::ORDER_ASCENDING;
 
 $query->run();
 
